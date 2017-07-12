12aa68e2b1b2adf7758670be16aa0580f89f1548
Darek <darooo87@gmail.com>
2017-07-12 23:48:36 +0100

    test

--- App2/App2/MainPage.xaml.cs
+++ App2/App2/MainPage.xaml.cs
@@ -14,9 +14,32 @@ namespace App2
         public MainPage()
         {
             InitializeComponent();
-
-            
         }
-        
+
+        private List<string> sentences = new List<string>()
+        {
+            "life is like riding a bicycle",
+            "to keep balance you must keep moving",
+            "sentence 3",
+            "sentence 54",
+            "sentence 65"
+        };
+
+        private int iterator = 0;
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            sentence.Text = sentences[iterator];
+
+            if(++iterator >= sentences.Count)
+            {
+                iterator = 0;
+            }
+        }
+
+        private void slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            slider.Value = Math.Round(slider.Value, 0);
+        }
     }
 }
