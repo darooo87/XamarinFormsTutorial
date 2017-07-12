04cc4e265b9beb9f6470e09812f1b9082e2bdf86
Dariusz Grabowski <d.grabowski@portico.com>
2017-07-12 09:21:30 +0100

    binding, onplatform

--- App2/App2/App.xaml.cs
+++ App2/App2/App.xaml.cs
@@ -13,8 +13,7 @@ namespace App2
 		{
 			InitializeComponent();
 
-            MainPage = new NavigationPage(new BlankPage());
-			//MainPage = new App2.MainPage();
+            MainPage = new MainPage();
 		}
 
 		protected override void OnStart ()
--- App2/App2/MainPage.xaml.cs
+++ App2/App2/MainPage.xaml.cs
@@ -11,31 +11,17 @@ namespace App2
 {
     public partial class MainPage : ContentPage
     {
-        public MainPage(string selected)
+        public MainPage()
         {
-            var layout = new StackLayout();
-            var editor = new Editor();
+            InitializeComponent();
 
-            var button = new Button();
-            button.Text = selected;
-            layout.Children.Add(editor);
-            layout.Children.Add(button);
-
-            button.Clicked += (sender, e) =>
-            {
-                if (editor != null && !string.IsNullOrEmpty(editor.Text))
-                {
-                    Person.People.Add(new Person()
-                    {
-                        Name = editor.Text,
-                        Title = "added"
-                    });
-                }
-                Navigation.PopAsync();
-            };
-
-
-            Content = layout;
+            //switch(Device.RuntimePlatform)
+            //{
+            //    case Device.iOS:
+            //    case Device.Android:
+            //        Padding = new Thickness(30, 30, 30, 30);
+            //        break;
+            //}
         }
     }
 }
